3d11522889c8a7a13ad1e2d9b31ad6c96e3aee6d
Nikolas Savvidis <nicos.savidis@gmail.com>
2020-04-04 08:25:49 +0200

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -15,7 +15,7 @@ export default {
   input: 'src/bundle.js',
   output: {
     file: 'dist/bundle.js',
-    format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+    format: 'iife',
     sourcemap: true
   },
   plugins: [
